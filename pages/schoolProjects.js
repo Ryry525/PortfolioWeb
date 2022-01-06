08b68991fffa0f88c17073ade54adfae3481f2ae
Ryry525 <ryanlin0228@gmail.com>
2022-01-06 10:15:25 -0500

    changes to school project page

--- pages/schoolProjects.js
+++ pages/schoolProjects.js
@@ -1,25 +1,31 @@
-import { Container, Heading, SimpleGrid } from "@chakra-ui/react"
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
+import Layout from '../components/layouts/article'
 import { SchoolWorkGridItem } from '../components/grid-item'
 
-import thumbAdventuregame from '../public/images/work/thumbAdventuregame.png'
+import thumbAdventuregame from '../public/images/work/Adventuregame.png'
 
-const schoolProjects = () => {
-    return (
+const schoolProjects = () => (
+  <Layout title="schoolProjects">
     <Container>
-        <Heading as="h3" fontSize={20} mb={4}>
-            School Works
-        </Heading>
+      <Heading as="h3" fontSize={20} mb={4}>
+        School Projects
+      </Heading>
 
-        <SimpleGrid columns={[1,1,2]} gap={6}>
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-        <SchoolWorkGridItem id="adventuregame" title="JS web adventure game" thumbnail={thumbAdventuregame}>
-            A simple web game made from JS; Part of THECODE: Javascript development program
-        </SchoolWorkGridItem>
+          <SchoolWorkGridItem
+            id="adventuregame"
+            title="Adventuregame"
+            thumbnail={thumbAdventuregame}
+          >
+            A simple web game made from JS; Part of THECODE: Javascript
+            development program
+          </SchoolWorkGridItem>
         </Section>
-        </SimpleGrid>
+      </SimpleGrid>
     </Container>
-    )
-}
+  </Layout>
+)
 
 export default schoolProjects
