c0447610b0cdc2e6196870eb42d7d1be147aa0e2
Ryry525 <ryanlin0228@gmail.com>
2022-01-06 10:18:03 -0500

    custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -9,19 +9,19 @@ import {
 } from '@chakra-ui/react'
 
 const NotFound = () => {
-    return (
-        <Container>
-            <Heading as="h1">Not found</Heading>
-            <Text> The page you&apos;re looking for was not found.</Text>
-            <Divider my={6} />
+  return (
+    <Container>
+      <Heading as="h1">Not found</Heading>
+      <Text> The page you&apos;re looking for was not found.</Text>
+      <Divider my={6} />
 
-            <Box my={6} align="center">
-            <NextLink href="/">
-                <Button colorScheme="red">Return to home</Button>
-            </NextLink>
-            </Box>
-        </Container>
-    )
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="red">Return to home</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
 }
 
 export default NotFound
